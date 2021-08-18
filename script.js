fd549d2922b3e5de267afde4920d6640c38bb5f4
bhuvisanathra <bhuvneshsanathara@gmail.com>
2021-08-18 21:03:41 +0530

    Added Comments!

--- script.js
+++ script.js
@@ -41,10 +41,14 @@ let data = [
 
 ]
 
+//Declaration of Const for Block
 const info = document.querySelector('#info');
 
+
+//Mapping
 let collection = data.map(function (data) {
     return '<div>' + data.name + ' is ' + data.age + ' Year Old' + '</div>';
 })
 
+//Joining
 info.innerHTML = collection.join('\n');
